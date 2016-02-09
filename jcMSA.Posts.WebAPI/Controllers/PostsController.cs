2a4e0fbe916c3f00ce02ef0987b4c6a41c930ba8
Jarred Capellman <jcapellman@hotmail.com>
2016-02-08 19:24:00 -0500

    Initial framework for Posts

---
+++ jcMSA.Posts.WebAPI/Controllers/PostsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jcMSA.Posts.WebAPI.Controllers {
+    public class PostsController : Controller {
+        public ActionResult Index() {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
