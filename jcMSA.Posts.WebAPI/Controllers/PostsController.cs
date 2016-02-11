b7466810a6dd1c4e3ed62310ada98682e7618fee
Jarred Capellman <jcapellman@hotmail.com>
2016-02-10 21:50:30 -0500

    Started migrating Posts WebAPI to .NET Core

--- jcMSA.Posts.WebAPI/Controllers/PostsController.cs
+++ jcMSA.Posts.WebAPI/Controllers/PostsController.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections.Generic;
-using System.Web.Mvc;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using jcMSA.Common.PCL.Transports.Container;
-using jcMSA.Posts.BusinessLogic.Managers;
 using jcMSA.Posts.PCL.Transports;
+using Microsoft.AspNet.Mvc;
 
-namespace jcMSA.Posts.WebAPI.Controllers {
-    public class PostsController : Controller {
-        public ReturnSet<List<PostListingResponseItem>> GET() {
-            return new PostListingManager().GetPostListing();
+namespace jcMSA.Posts.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        public ReturnSet<List<PostListingResponseItem>> GET()
+        {
+            return null;
         }
     }
 }
--- jcMSA.Posts.WebAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jcMSA.Posts.WebAPI.WebApiApplication" Language="C#" %>
