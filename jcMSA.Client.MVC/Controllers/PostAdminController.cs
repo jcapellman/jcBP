cf4d71263bb1cf24fdfdf9f2823b71123721beb3
jcapellman <jcapellman@hotmail.com>
2016-02-26 21:22:59 -0500

    Initial work on the PostAdmin

---
+++ jcMSA.Client.MVC/Controllers/PostAdminController.cs
@@ -0,0 +1,7 @@
+﻿using System.Web.Mvc;
+
+namespace jcMSA.Client.MVC.Controllers {
+    public class PostAdminController : BaseController {
+        public ActionResult Index() { return View(); }
+    }
+}
