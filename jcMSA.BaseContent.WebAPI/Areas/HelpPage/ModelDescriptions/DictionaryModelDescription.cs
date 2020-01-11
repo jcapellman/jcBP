8e323209061bad99cca669bc9acb14044d273b1c
Jarred Capellman <jcapellman@hotmail.com>
2020-01-11 10:15:56 -0600

    Change: Updated to .NET 4.8, fixed the CVE and updated the .gitignore

---
+++ jcMSA.BaseContent.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace jcMSA.BaseContent.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ jcMSA.Posts.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace jcMSA.Posts.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
